0afa2e6a784957e3535f5b600276a3052a38a68e
maki <maki@MAKINO-PC>
2023-08-04 02:54:35 +0900

    20230804 junitはクラスパスを通す形式に変更（モジュールがjunit依存になってしまうことの回避）

--- src/module-info.java
+++ src/module-info.java
@@ -1,3 +1,8 @@
 module pen_study_module_system_lib {
-    requires junit;
+    // 1つ1つexportする必要がある
+    // 本来公開するべきパッケージ
+    exports pen.study.core;
+
+    // 公開したくない内部用パッケージ
+    // exports pen.study.internal.util;
 }
